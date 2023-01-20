33deb0710dda87f3c02a1d9eeef7f94c2515e2d0
Matilda Gantner <matpuch@yahoo.de>
2023-01-20 12:11:05 +0100

    Read events from json implemented

--- eventDetailWindow.mjs
+++ eventDetailWindow.mjs
@@ -1,5 +1,26 @@
 
+let events = [];
+fetch('./sportData.json')
+    .then((response) => response.json())
+    .then(data => {
+        events = data;
+    });
+
 // Opens new window that shows all details of all events at date x
 export function detailWindow(x) {
+    const detailWindow = window.open();
+    detailWindow.document.body.innerHTML = "<div>Test Test </div>";
+
+    let eventsToday = [];
+
+    for (let i = 0; i < Object.keys(events["data"]).length; i++) {
+        if (events["data"][i]["status"].localeCompare("scheduled") === 0) {
+            eventsToday.push(events["data"][i]);
+        }
+    }
+
+    console.log(eventsToday);
+
+
     return x;
 }
